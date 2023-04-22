2fd47209f521802a764838cce79a6bca892f6bb4
Huo_s <Huo_s@Sokkheang>
2023-04-22 15:50:01 +0700

    clean code

--- src/main/java/com/solo/controller/ReportController.java
+++ src/main/java/com/solo/controller/ReportController.java
@@ -22,170 +22,117 @@ import net.sf.jasperreports.engine.JRException;
 
 @RestController
 public class ReportController {
-	
+
 	@GetMapping("/pdf")
-	public void pdf(HttpServletResponse response) throws JRException, SException, IOException{
+	public void pdf(HttpServletResponse response) throws JRException, SException, IOException {
 		Solo params = new Solo();
-		
-		SList list = new SList();
-		
-		for(int i=1 ; i<=10; i++) {
-			Solo solo = new Solo();
-			solo.setInt("id", i);
-			solo.setString("pname", "product " + i);
-			solo.setInt("unit", i + 1);
-			solo.setString("price", i * 2 + " USD");
-			list.add(solo);
-		}		
-		params.set("myCollection", JasperReportUtil.setDataSource(list));
+
+		params.set("myCollection", JasperReportUtil.setDataSource(getList()));
 		params.setString("title", "Solo Framework");
 		params.setString("name", "Solo Name");
 		params.setString("age", "18 years old");
-		
-		byte[] pdf = JasperReportUtil.ExportReport(JasperReportUtil.PDF, params, "jasper/test", "pdfFileName").getBody();
+
+		byte[] pdf = JasperReportUtil.ExportReport(JasperReportUtil.PDF, params, "jasper/test", "pdfFileName")
+				.getBody();
 		response.setContentType("application/pdf;charset=UTF-8");
-        StreamUtils.copy(pdf, response.getOutputStream());
+		StreamUtils.copy(pdf, response.getOutputStream());
 	}
-	
+
 	@GetMapping("/image")
-	public void image(HttpServletResponse response) throws JRException, SException, IOException{
+	public void image(HttpServletResponse response) throws JRException, SException, IOException {
 		Solo params = new Solo();
-		
-		SList list = new SList();
-		
-		for(int i=1 ; i<=10; i++) {
-			Solo solo = new Solo();
-			solo.setInt("id", i);
-			solo.setString("pname", "product " + i);
-			solo.setInt("unit", i + 1);
-			solo.setString("price", i * 2 + " USD");
-			list.add(solo);
-		}		
-		params.set("myCollection", JasperReportUtil.setDataSource(list));
+
+		params.set("myCollection", JasperReportUtil.setDataSource(getList()));
 		params.setString("title", "Solo Framework");
 		params.setString("name", "Solo Name");
 		params.setString("age", "18 years old");
-		
-		byte[] image = JasperReportUtil.ExportReport(JasperReportUtil.IMAGE, params, "jasper/test", "imageFileName").getBody();
-		
+
+		byte[] image = JasperReportUtil.ExportReport(JasperReportUtil.IMAGE, params, "jasper/test", "imageFileName")
+				.getBody();
+
 		response.setContentType(MediaType.IMAGE_JPEG_VALUE);
-        StreamUtils.copy(image, response.getOutputStream());
+		StreamUtils.copy(image, response.getOutputStream());
 	}
-	
+
 	@GetMapping("/html")
-	public byte[] html() throws JRException, SException, IOException{
+	public byte[] html() throws JRException, SException, IOException {
 		Solo params = new Solo();
-		
-		SList list = new SList();
-		
-		for(int i=1 ; i<=10; i++) {
-			Solo solo = new Solo();
-			solo.setInt("id", i);
-			solo.setString("pname", "product " + i);
-			solo.setInt("unit", i + 1);
-			solo.setString("price", i * 2 + " USD");
-			list.add(solo);
-		}		
-		params.set("myCollection", JasperReportUtil.setDataSource(list));
+
+		params.set("myCollection", JasperReportUtil.setDataSource(getList()));
 		params.setString("title", "Solo Framework");
 		params.setString("name", "Solo Name");
 		params.setString("age", "18 years old");
 
 		return JasperReportUtil.ExportReport(JasperReportUtil.HTML, params, "jasper/test", "htmlFileName").getBody();
 	}
-	
+
 	@GetMapping("/htmlToPdf")
-	public void htmlToPdf(HttpServletResponse response) throws JRException, SException, IOException, DocumentException{
+	public void htmlToPdf(HttpServletResponse response) throws JRException, SException, IOException, DocumentException {
 		Solo params = new Solo();
-		
-		SList list = new SList();
-		
-		for(int i=1 ; i<=10; i++) {
-			Solo solo = new Solo();
-			solo.setInt("id", i);
-			solo.setString("pname", "product " + i);
-			solo.setInt("unit", i + 1);
-			solo.setString("price", i * 2 + " USD");
-			list.add(solo);
-		}		
-		params.set("myCollection", JasperReportUtil.setDataSource(list));
+
+		params.set("myCollection", JasperReportUtil.setDataSource(getList()));
 		params.setString("title", "Solo Framework");
 		params.setString("name", "Solo Name");
 		params.setString("age", "18 years old");
 
-		byte[] html = JasperReportUtil.ExportReport(JasperReportUtil.HTML, params, "jasper/test", "htmlFileName").getBody();
-		
+		byte[] html = JasperReportUtil.ExportReport(JasperReportUtil.HTML, params, "jasper/test", "htmlFileName")
+				.getBody();
+
 		ByteArrayInputStream byteArrayInputStream = JasperReportUtil.convertHtmlToPdf(new String(html));
 		response.setContentType("application/pdf");
-        response.setHeader("Content-Disposition", "filename=htmlToPdf.pdf");
-        IOUtils.copy(byteArrayInputStream, response.getOutputStream());
-	    
+		response.setHeader("Content-Disposition", "filename=htmlToPdf.pdf");
+		IOUtils.copy(byteArrayInputStream, response.getOutputStream());
+
 	}
-	
+
 	@GetMapping("/excel")
-	public ResponseEntity<byte[]> excel() throws JRException, SException, IOException{
+	public ResponseEntity<byte[]> excel() throws JRException, SException, IOException {
 		Solo params = new Solo();
-		
-		SList list = new SList();
-		
-		for(int i=1 ; i<=10; i++) {
-			Solo solo = new Solo();
-			solo.setInt("id", i);
-			solo.setString("pname", "product " + i);
-			solo.setInt("unit", i + 1);
-			solo.setString("price", i * 2 + " USD");
-			list.add(solo);
-		}		
-		params.set("myCollection", JasperReportUtil.setDataSource(list));
+
+		params.set("myCollection", JasperReportUtil.setDataSource(getList()));
 		params.setString("title", "Solo Framework");
 		params.setString("name", "Solo Name");
 		params.setString("age", "18 years old");
-		
+
 		return JasperReportUtil.ExportReport(JasperReportUtil.EXCEL, params, "jasper/test", "ExcelFileName");
 	}
-	
+
 	@GetMapping("/word")
-	public ResponseEntity<byte[]> word() throws JRException, SException, IOException{
+	public ResponseEntity<byte[]> word() throws JRException, SException, IOException {
 		Solo params = new Solo();
-		
-		SList list = new SList();
-		
-		for(int i=1 ; i<=10; i++) {
-			Solo solo = new Solo();
-			solo.setInt("id", i);
-			solo.setString("pname", "product " + i);
-			solo.setInt("unit", i + 1);
-			solo.setString("price", i * 2 + " USD");
-			list.add(solo);
-		}		
-		params.set("myCollection", JasperReportUtil.setDataSource(list));
+
+		params.set("myCollection", JasperReportUtil.setDataSource(getList()));
 		params.setString("title", "Solo Framework");
 		params.setString("name", "Solo Name");
 		params.setString("age", "18 years old");
-		
+
 		return JasperReportUtil.ExportReport(JasperReportUtil.WORD, params, "jasper/test", "wordFileName");
 	}
-	
+
 	@GetMapping("/powerpoint")
-	public ResponseEntity<byte[]> powerpoint() throws JRException, SException, IOException{
+	public ResponseEntity<byte[]> powerpoint() throws JRException, SException, IOException {
 		Solo params = new Solo();
-		
+
+		params.set("myCollection", JasperReportUtil.setDataSource(getList()));
+		params.setString("title", "Solo Framework");
+		params.setString("name", "Solo Name");
+		params.setString("age", "18 years old");
+
+		return JasperReportUtil.ExportReport(JasperReportUtil.POWERPOINT, params, "jasper/test", "powerPointFileName");
+	}
+	
+	private SList getList() {
 		SList list = new SList();
-		
-		for(int i=1 ; i<=10; i++) {
+
+		for (int i = 1; i <= 10; i++) {
 			Solo solo = new Solo();
 			solo.setInt("id", i);
 			solo.setString("pname", "product " + i);
 			solo.setInt("unit", i + 1);
 			solo.setString("price", i * 2 + " USD");
 			list.add(solo);
-		}		
-		params.set("myCollection", JasperReportUtil.setDataSource(list));
-		params.setString("title", "Solo Framework");
-		params.setString("name", "Solo Name");
-		params.setString("age", "18 years old");
-		
-		return JasperReportUtil.ExportReport(JasperReportUtil.POWERPOINT, params, "jasper/test", "powerPointFileName");
+		}
+		return list;
 	}
 
 }

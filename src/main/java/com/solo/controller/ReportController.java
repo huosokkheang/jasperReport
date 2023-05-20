4ae23bb5497d53781339e15d53c3393a2c75e895
Huo_s <sokkheanghuo@gmail.com.com>
2023-05-20 14:38:03 +0700

    fix resolution export image

--- src/main/java/com/solo/controller/ReportController.java
+++ src/main/java/com/solo/controller/ReportController.java
@@ -1,11 +1,9 @@
 package com.solo.controller;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.pdfbox.io.IOUtils;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StreamUtils;
@@ -16,39 +14,27 @@ import com.core.collection.SList;
 import com.core.collection.Solo;
 import com.core.exception.business.SException;
 import com.core.util.Jasper.JasperReportUtil;
-import com.lowagie.text.DocumentException;
 
 import net.sf.jasperreports.engine.JRException;
+
 
 @RestController
 public class ReportController {
 
 	@GetMapping("/pdf")
 	public void pdf(HttpServletResponse response) throws JRException, SException, IOException {
-		Solo params = new Solo();
-
-		params.set("myCollection", JasperReportUtil.setDataSource(getList()));
-		params.setString("title", "Solo Framework");
-		params.setString("name", "Solo Name");
-		params.setString("age", "18 years old");
-
-		byte[] pdf = JasperReportUtil.ExportReport(JasperReportUtil.PDF, params, "jasper/test", "pdfFileName")
-				.getBody();
+		Solo params = getList();
+		
+		byte[] pdf = JasperReportUtil.ExportReport(JasperReportUtil.PDF, params, "jasper/test", "pdfFileName").getBody();
 		response.setContentType("application/pdf;charset=UTF-8");
 		StreamUtils.copy(pdf, response.getOutputStream());
 	}
 
 	@GetMapping("/image")
 	public void image(HttpServletResponse response) throws JRException, SException, IOException {
-		Solo params = new Solo();
+		Solo params = getList();
 
-		params.set("myCollection", JasperReportUtil.setDataSource(getList()));
-		params.setString("title", "Solo Framework");
-		params.setString("name", "Solo Name");
-		params.setString("age", "18 years old");
-
-		byte[] image = JasperReportUtil.ExportReport(JasperReportUtil.IMAGE, params, "jasper/test", "imageFileName")
-				.getBody();
+		byte[] image = JasperReportUtil.ExportReport(JasperReportUtil.IMAGE, params, "jasper/test", "imageFileName").getBody();
 
 		response.setContentType(MediaType.IMAGE_JPEG_VALUE);
 		StreamUtils.copy(image, response.getOutputStream());
@@ -56,72 +42,34 @@ public class ReportController {
 
 	@GetMapping("/html")
 	public byte[] html() throws JRException, SException, IOException {
-		Solo params = new Solo();
-
-		params.set("myCollection", JasperReportUtil.setDataSource(getList()));
-		params.setString("title", "Solo Framework");
-		params.setString("name", "Solo Name");
-		params.setString("age", "18 years old");
+		Solo params = getList();
 
 		return JasperReportUtil.ExportReport(JasperReportUtil.HTML, params, "jasper/test", "htmlFileName").getBody();
 	}
 
-	@GetMapping("/htmlToPdf")
-	public void htmlToPdf(HttpServletResponse response) throws JRException, SException, IOException, DocumentException {
-		Solo params = new Solo();
-
-		params.set("myCollection", JasperReportUtil.setDataSource(getList()));
-		params.setString("title", "Solo Framework");
-		params.setString("name", "Solo Name");
-		params.setString("age", "18 years old");
-
-		byte[] html = JasperReportUtil.ExportReport(JasperReportUtil.HTML, params, "jasper/test", "htmlFileName")
-				.getBody();
-
-		ByteArrayInputStream byteArrayInputStream = JasperReportUtil.convertHtmlToPdf(new String(html));
-		response.setContentType("application/pdf");
-		response.setHeader("Content-Disposition", "filename=htmlToPdf.pdf");
-		IOUtils.copy(byteArrayInputStream, response.getOutputStream());
-
-	}
-
 	@GetMapping("/excel")
 	public ResponseEntity<byte[]> excel() throws JRException, SException, IOException {
-		Solo params = new Solo();
-
-		params.set("myCollection", JasperReportUtil.setDataSource(getList()));
-		params.setString("title", "Solo Framework");
-		params.setString("name", "Solo Name");
-		params.setString("age", "18 years old");
+		Solo params = getList();
 
 		return JasperReportUtil.ExportReport(JasperReportUtil.EXCEL, params, "jasper/test", "ExcelFileName");
 	}
 
 	@GetMapping("/word")
 	public ResponseEntity<byte[]> word() throws JRException, SException, IOException {
-		Solo params = new Solo();
-
-		params.set("myCollection", JasperReportUtil.setDataSource(getList()));
-		params.setString("title", "Solo Framework");
-		params.setString("name", "Solo Name");
-		params.setString("age", "18 years old");
+		Solo params = getList();
 
 		return JasperReportUtil.ExportReport(JasperReportUtil.WORD, params, "jasper/test", "wordFileName");
 	}
 
 	@GetMapping("/powerpoint")
 	public ResponseEntity<byte[]> powerpoint() throws JRException, SException, IOException {
-		Solo params = new Solo();
-
-		params.set("myCollection", JasperReportUtil.setDataSource(getList()));
-		params.setString("title", "Solo Framework");
-		params.setString("name", "Solo Name");
-		params.setString("age", "18 years old");
+		Solo params = getList();
 
 		return JasperReportUtil.ExportReport(JasperReportUtil.POWERPOINT, params, "jasper/test", "powerPointFileName");
 	}
 	
-	private SList getList() {
+	private Solo getList() {
+		Solo params = new Solo();
 		SList list = new SList();
 
 		for (int i = 1; i <= 10; i++) {
@@ -132,7 +80,11 @@ public class ReportController {
 			solo.setString("price", i * 2 + " USD");
 			list.add(solo);
 		}
-		return list;
+		params.set("myCollection", JasperReportUtil.setDataSource(list));
+		params.setString("title", "Solo Framework");
+		params.setString("name", "Solo Name");
+		params.setString("age", "18 years old");
+		return params;
 	}
 
 }
